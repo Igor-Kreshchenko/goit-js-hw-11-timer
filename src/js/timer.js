aaa5856bbc61cae803cb886d0b74456d1c32eccf
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-02-12 11:00:41 +0200

    Update timer.js

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,4 +1,49 @@
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
+// new CountdownTimer({
+//   selector: '#timer-1',
+//   targetDate: new Date('Jul 17, 2019'),
+// });
+
+const refs = {
+  daysRef: document.querySelector('[data-value="days"]'),
+  hoursRef: document.querySelector('[data-value="hours"]'),
+  minsRef: document.querySelector('[data-value="mins"]'),
+  secsRef: document.querySelector('[data-value="secs"]'),
+};
+
+const timer = {
+  start() {
+    const startTime = Date.now();
+
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+
+      updateClockFace({ days, hours, mins, secs });
+    }, 1000);
+  },
+};
+
+timer.start();
+
+function getTimeComponents(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+  return { days, hours, mins, secs };
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateClockFace({ days, hours, mins, secs }) {
+  refs.daysRef.textContent = `${days}`;
+  refs.hoursRef.textContent = `${hours}`;
+  refs.minsRef.textContent = `${mins}`;
+  refs.secsRef.textContent = `${secs}`;
+}
